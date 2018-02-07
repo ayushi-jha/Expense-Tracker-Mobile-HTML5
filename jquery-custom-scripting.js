5f0ca005f45d2ce7b95510f99309ec2bd7e75bb7
Ayushi Jha <jhaayushijha@gmail.com>
2018-02-07 10:29:21 +0530

    Including income tracker in the application

---
+++ jquery-custom-scripting.js
@@ -0,0 +1,44 @@
+$(document).bind("mobileinit", function(){
+	$.mobile.page.prototype.options.addBackBtn = true;
+	$.mobile.defaultPageTransition = 'none';
+	// Initilize Web SQL Database
+	db = window.openDatabase("ExpenseTrackerDemo", "1.0", "Expense Tracker Demo Database", 200000);
+	db.transaction(function(tx) {
+		tx.executeSql("CREATE TABLE IF NOT EXISTS expenses (expenseID INTEGER PRIMARY KEY, expenseType TEXT, expenseAmount INTEGER, expenseDate TEXT)");
+		tx.executeSql("CREATE TABLE IF NOT EXISTS income (incomeID INTEGER PRIMARY KEY, incomeType TEXT, incomeAmount INTEGER, incomeDate TEXT)");
+	});
+});
+
+// Global declarations
+var contentMainVar = null;
+var expenseTypeVar = null;
+var expenseAmountVar = null;
+var expenseDateVar = null;
+var incomeTypeVar = null;
+var incomeAmountVar = null;
+var incomeDateVar = null;
+var contentDialogVar = null;
+var contentMessageVar = null;
+var contentOKVar = null;
+var confirmationOKVar = null;
+var d = new Date();
+var curr_date = d.getDate();
+var curr_month = d.getMonth() + 1;
+var curr_year = d.getFullYear();
+var date_string = curr_month + "/" + curr_date + "/" + curr_year;
+
+// Constants
+var EMPTY = "";
+
+function resetDB() {
+	db.transaction(function(tx) {
+		tx.executeSql('DROP TABLE IF EXISTS expenses', [], function() {
+			document.location = 'index.html';
+		});
+	});
+}
+
+function formatCurrency(num) {
+	num = isNaN(num) || num === '' || num === null ? 0.00 : num;
+	return parseFloat(num).toFixed(2);
+}
